b6d16f1a4355a2690a597296a5ffffa48864dddf
vulogov <vladimir.ulogov@me.com>
2017-05-05 10:02:39 -0400

    Updating Road Map for 0.7

--- zq/zq_query_loop.pyx
+++ zq/zq_query_loop.pyx
@@ -1,2 +1,10 @@
+def _QLoop(ctx, _dict, args, kw):
+    return ctx
+
+def _Loop(ctx, _fun, args, kw):
+    while True:
+        pass
+
 def Loop(ctx, *args, **kw):
+    _Loop(ctx, _QLoop, args, kw)
     return ctx
