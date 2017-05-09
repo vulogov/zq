e037a4a80c9d67cec8ab02add03db4fe60e4069b
vulogov <vladimir.ulogov@me.com>
2017-05-09 13:29:21 -0400

    (Trends)

--- Documentation/ReleaseNotes/ReleaseNote_0.7.md
+++ Documentation/ReleaseNotes/ReleaseNote_0.7.md
@@ -16,6 +16,17 @@ Example:
 ```
 This query, will select the host with name "MyHost", then perform a (Join) only for the ITEM. Then perform (History) get.
 
+2. Trends query word: (Trends) This word will pull the result of the (Join) or (Items) from the Stack and perform a trend query. 
+Example:
+```bash
+(ZBX) 
+    (Hosts :filter {"host" ["MyTest"]}) 
+    (Join :TRIGGER False :HOSTGROUPS False :TEMPLATE False :INTERFACE False) 
+    (Filter TRUE ["name" Eq "Incoming connections"]) 
+    (Trends :time_from (Date.Time "12 hours ago") :time_till (Date.Time "now"))
+(Out)
+```
+This query, will select the host with name "MyHost", then perform a (Join) only for the ITEM. Then perform (Trends) get.
 
 
 ### Standard modules library
--- zq/zq_query_trends.pyx
+++ zq/zq_query_trends.pyx
@@ -1,2 +1,32 @@
 def Trends(ctx, *args, **kw):
+    kw["output"] = "extend"
+    stamp = time.time()
+    if not kw.has_key("time_from"):
+        kw["time_from"] = stamp - 3600
+    if not kw.has_key("time_till"):
+        kw["time_till"] = stamp
+    if kw["time_till"] <= kw["time_from"]:
+        if ctx.env.shell != None:
+            ctx.env.shell.error("Invalid time range for the (Trend)")
+        return ctx
+    out = []
+    _done = []
+    while True:
+        p = ctx.pull()
+        if not p:
+            break
+        if not p.has_key("ITEM"):
+            ctx.push(p)
+            break
+        for i in p["ITEM"]:
+            if int(i["status"]) != 0:
+                continue
+            if i["itemid"] in _done:
+                continue
+            _kw = copy.copy(kw)
+            _kw["itemids"] = [i["itemid"]]
+            res = apply(ctx.zapi.trend.get, args, _kw)
+            out.append(res)
+            _done.append(i["itemid"])
+    Push(ctx, "TRENDS", out)
     return ctx
